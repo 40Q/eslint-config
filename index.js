b475c9ee3609882e7df727304aa0c5596d4691ce
Maximo Martinez Soria <mmartinez@40q.agency>
2023-11-27 15:35:53 -0300

    Include prettier

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    '@40q/eslint-config'
+    '@40q/eslint-config',
+    'prettier',
   ],
   rules: {
     'semi': 'error',
